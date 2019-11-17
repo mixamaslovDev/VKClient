0739d690f67f185a5fefa5f52c095d2c7cddc718
mixamaslovDev <mixamaslov@mail.ru>
2019-11-17 23:57:33 +0300

    done logout function

--- VKClient/Controllers/LogOut.swift
+++ VKClient/Controllers/LogOut.swift
@@ -7,37 +7,69 @@
 //
 
 import UIKit
+import WebKit
+import RealmSwift
 
 class LogOutContoller: UIViewController {
     
+    let realm = RealmDel()
     
     @IBAction func logOut(_ sender: UIButton)  {
-        clear(cache: true, cookies: true)
-        Session.shared.token = nil
-        Session.shared.userId = nil
-        
+        alertExit()
+//        clear(cache: true, cookies: true)
+//        Session.shared.token = nil
+//        Session.shared.userId = nil
+//        realm.deleteData()
+//        clean()
     }
     
     func clear(cache: Bool, cookies: Bool) {
         if cache { clearCache() }
         if cookies { clearCookies() }
     }
-
+    
     fileprivate func clearCache() {
         URLCache.shared.removeAllCachedResponses()
         URLCache.shared.diskCapacity = 0
         URLCache.shared.memoryCapacity = 0
         
     }
-
+    
     fileprivate func clearCookies() {
         let cookieStorage = HTTPCookieStorage.shared
-
+        
         guard let cookies = cookieStorage.cookies else { return }
-
+        
         for cookie in cookies {
             cookieStorage.deleteCookie(cookie)
         }
+    }
+    
+    func clean() {
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+            }
+        }
+    }
+    
+    func alertExit() {
+        
+        let alert = UIAlertController(title: "Выход из аккаунта", message: "Вы уверены, что хотите выйти?", preferredStyle: UIAlertController.Style.alert)
+        
+        alert.addAction(UIAlertAction(title: "Выйти", style: UIAlertAction.Style.default, handler: { action in
+            self.clear(cache: true, cookies: true)
+            Session.shared.token = nil
+            Session.shared.userId = nil
+            self.realm.deleteData()
+            self.clean()
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Отмена", style: UIAlertAction.Style.cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+        
     }
     
     
--- VKClient/Realm/RealmAdding.swift
+++ VKClient/Realm/RealmAdding.swift
@@ -35,4 +35,5 @@ class ReamlAdd {
             print(error)
         }
     }
+    
 }
